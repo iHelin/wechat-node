5c893f3109b359215c6ed34f248dfe7fcb75e3b7
iHelin <ihelin@outlook.com>
2021-01-29 20:45:46 +0800

    update

--- src/reply/ok.js
+++ src/reply/ok.js
@@ -5,6 +5,7 @@ module.exports = {
         let url = `http://poetry.apiopen.top/sentences`;
         return rp({method: 'GET', url, json: true});
     },
+
     hitokoto() {
         let url = `https://api.imjad.cn/hitokoto`;
         return rp({method: 'GET', url, json: true});
